ec637d78b6f2e4f986f86fd6b9a04a8da167fb77
Komphet <komphet.me@gmail.com>
2020-04-25 02:50:43 +0700

    Update Props and State

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import "./css/style.css";
 import FoxComponent from './components/FoxComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="background">
-        <FoxComponent/>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      foxStatus: "standing"
+    };
+  }
+
+  keyDownHandler = (e)=>{
+    if(this.state.foxStatus === "standing"){
+      this.setState({
+        foxStatus: "running"
+      });
+    }
+  }
+  
+  render(){
+    return (
+      <div className={`background ${this.state.foxStatus === "running" && "background-running"}`} tabIndex="0" onKeyDown={this.keyDownHandler}>
+        <FoxComponent status={this.state.foxStatus}/>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/FoxComponent.js
+++ src/components/FoxComponent.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class FoxComponent extends React.Component {
     render(){
-        return <div className="fox-object fox-standing"></div>
+        return <div className={`fox-object ${this.props.status === "standing" ? "fox-standing" : "fox-running"}`}></div>
     }
 }
