3e69de210e598a3e59dc0e3324de0374acb91855
Komphet <komphet.me@gmail.com>
2020-04-25 05:22:23 +0700

    Update API

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import {
-    Card, CardBody, Button,Container, Row, Col,Input
-  } from 'reactstrap';
+    Card, CardBody, Button, Container, Row, Col, Input
+} from 'reactstrap';
+import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
 
 export default class FormComponent extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             name: ""
         }
     }
 
-    __onChageHandler = (e)=>{
+    __onChageHandler = (e) => {
         this.setState({
             name: e.target.value
         });
@@ -21,14 +23,35 @@ export default class FormComponent extends Component {
         return (
             <Container>
                 <Row>
-                    <Col sm={{size: 8, offset: 2}}>
-                        <div style={{marginTop: 100}}>
+                    <Col sm={{ size: 8, offset: 2 }}>
+                        <div style={{ marginTop: 100 }}>
                             <Card>
                                 <CardBody>
-                                    <Input onChange={this.__onChageHandler} value={this.state.name}  style={{marginBottom: 10}} type="text" name="name" placeholder="Your Name" />
-                                    <Button onClick={()=>{
-                                        this.props.setFoxName(this.state.name);
-                                    }} color="primary" style={{width: "100%"}}>Let's start!</Button>
+                                    <Input onChange={this.__onChageHandler} value={this.state.name} style={{ marginBottom: 10 }} type="text" name="name" placeholder="Your Name" />
+                                    <Row>
+                                        <Col sm={6}>
+                                            <Button onClick={() => {
+                                                this.props.setFoxName(this.state.name);
+                                            }} color="primary" style={{ width: "100%" }}>Let's start!</Button>
+                                        </Col>
+                                        <Col sm={6}>
+                                            <FacebookLogin
+                                                appId="2658551874377386"
+                                                callback={(res) => {
+                                                    axios.get("https://graph.facebook.com/v6.0/me?fields=id%2Cfirst_name&access_token=" + res.accessToken).then((resProfile) => {
+                                                        console.log(resProfile.data);
+                                                        this.props.setFoxName(resProfile.data.first_name);
+                                                    });
+
+                                                }}
+                                                cssClass="btn btn-primary btn-facebook"
+                                                icon="fa-facebook"
+                                            />
+                                        </Col>
+                                    </Row>
+
+
+
                                 </CardBody>
                             </Card>
                         </div>
